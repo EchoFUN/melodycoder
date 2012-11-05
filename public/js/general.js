c34f6c20dd43e0dbfd52dae6949b3b484ecf18a1
徐楷 <xukai.ken@gmail.com>
2012-11-05 14:46:26 +0800

    complete the front end stuctor

--- public/js/general.js
+++ public/js/general.js
@@ -1,3 +1,14 @@
-document.observe("dom:loaded", function() {
-	
+/**
+ * @fileverview 整个站点的入口执行js文件
+ * @author xukai.ken@gmail.com
+ */
+
+seajs.config({
+	alias: {
+		'prototype': 'http://127.0.0.1:3000/js/libs/prototype.js'
+	}
+});
+
+seajs.use(YYMG.modules, function(module) {
+	module.init();
 });
---
+++ public/js/modules/blog.js
@@ -0,0 +1,14 @@
+/**
+ * @fileoverview 日志模块的执行
+ * @author xukai.ken@gmail.com
+ */
+
+define(function(require, exports, module) {
+	exports.init = function() {
+		alert('test~');
+	}
+	
+	exports.listInit = function() {
+		
+	}
+});
