7da361043a18e213799c7332519701cb3c707c38
EchoFUN <xukai.ken@gmail.com>
2012-11-15 23:41:48 +0800

    disable the log module.

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.configure(function() {
     app.set('views', __dirname + '/views');
     app.set('view engine', 'ejs');
     app.use(express.favicon());
-    app.use(express.logger('dev'));
+    // app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
     app.use(app.router);
