e589b0562ff431742e9006ccc41be2d5dc89e47a
unknown <kai.xu@.(none)>
2012-11-15 14:56:28 +0800

    add the template module.

---
+++ public/js/common/tpl.js
@@ -0,0 +1,22 @@
+/**
+ * @fileverview 前端模板文件
+ * @author xukai.ken@gmail.com
+ */
+
+define(function(require, exports, module) {
+	
+	// 评论模板
+	exports.commentTPL = '<div class="comment odd" data-rid="50a48a4c9021389c16000001">\
+	    					<div class="avatar">\
+	    						<img height="38" width="38" src="">\
+	    					</div>\
+    						<div class="comment-info">\
+	    						<div class="name"><a href="undefined">亲爱的爸爸妈妈</a>&nbsp;说：</div>\
+	    						<div class="date">Nov&nbsp;15,&nbsp;2012&nbsp;at&nbsp;14:23</div>\
+	    					</div>\
+	    					<div class="clear"></div>\
+	    					<div class="comment-content">你们好吗？</div>\
+	    				</div>';
+	    				
+	
+});
