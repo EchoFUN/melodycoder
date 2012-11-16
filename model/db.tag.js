09bc80cd7f104b3f464e8ca95d00c962572ef71d
XU Kai <xukai.ken@gmail.com>
2012-11-17 00:13:07 +0800

    get the data for tag cloud.

--- model/db.tag.js
+++ model/db.tag.js
@@ -1,5 +1,21 @@
 /**
  * @fileoverview 系统中关于标签的操作
  * @version 2012.10.03
- *
  */
+
+exports.getWidgetTags = function(callback) {
+	var Tag = db.models.Tag;
+	Tag.find().exec(function(error, oriTags) {
+		var tags = [];
+		for (var i = 0; i < oriTags.length; i++) {
+			_t = oriTags[i];
+			var _has = false;
+			for(var j=0; j<tags.length; j++) {
+				if(tags[j].title == _t.title) {
+					;
+				}
+			}
+		}
+		callback(tags);
+	});
+}
