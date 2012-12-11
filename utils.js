19a7389cde32d5785e0eab19f14b12f7e1d1cb6c
EchoFUN <xukai.ken@gmail.com>
2012-12-11 20:14:58 +0800

    judge the version for different browsers.

--- utils.js
+++ utils.js
@@ -10,15 +10,33 @@
  * @param {object} options 需要拷贝的目标对象
  */
 exports.extend = function(target, options) {
-    for (var i in options) {
-        var copy = options[i];
-        if (copy instanceof Array) {
-            target[i] = this.extend(target[i], copy);
-        } else if (copy instanceof Object) {
-            target[i] = this.extend(target[i], copy);
-        } else {
-            target[i] = copy;
-        }
-    }
-    return target;
+	for (var i in options) {
+		var copy = options[i];
+		if ( copy instanceof Array) {
+			target[i] = this.extend(target[i], copy);
+		} else if ( copy instanceof Object) {
+			target[i] = this.extend(target[i], copy);
+		} else {
+			target[i] = copy;
+		}
+	}
+	return target;
+}
+/**
+ * @description 根据浏览器的版本做判断
+ * @param {string} ua
+ */
+exports.browser = function(ua) {
+	var reg = {
+		webkit : /(webkit)[ \/]([\w.]+)/,
+		opera : /(opera)(?:.*version)?[ \/]([\w.]+)/,
+		msie : /(msie) ([\w.]+)/,
+		mozilla : /(mozilla)(?:.*? rv:([\w.]+))?/
+	}
+	ua = ua.toLowerCase();
+	var match = reg.webkit.exec(ua) || reg.opera.exec(ua) || reg.msie.exec(ua) || ua.indexOf("compatible") < 0 && reg.mozilla.exec(ua) || [];
+	return {
+		browser : match[1] || "",
+		version : match[2] || "0"
+	};
 }
