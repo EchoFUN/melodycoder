711101744c6fcdb29e9924d1f49bccac96d89f13
EchoFUN <xukai.ken@gmail.com>
2012-12-26 20:10:49 +0800

    decodeURIComponent function has a size limit.

--- routes/post.js
+++ routes/post.js
@@ -81,7 +81,7 @@ exports.publishPost = function(req, resp) {
 		}
 	}
 	try {
-		var postData = JSON.parse(decodeURIComponent(req.body.r));
+		var postData = JSON.parse(req.body.r);
 	} catch (e) {
 		throw Error('Error parsing the request.');
 	}
