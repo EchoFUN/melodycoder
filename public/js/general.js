7ddd6618060e212696a6506b0e2c47c59bd0cc35
EchoFUN <xukai.ken@gmail.com>
2013-01-06 15:14:17 +0800

    add a simple error Handler.

--- public/js/general.js
+++ public/js/general.js
@@ -6,25 +6,22 @@
 seajs.config({
 	alias : {
 		'prototype' : YYMG.staticUrl + '/js/libs/prototype.js',
+		'base' : YYMG.staticUrl + '/js/modules/base.js'
 	}
 });
 
 YYMG.modules.push('prototype');
-YYMG.modules.push('./js/modules/base');
+YYMG.modules.push('base');
 seajs.use(YYMG.modules, function() {
 	var arg = arguments, argLen = arg.length;
 	document.observe("dom:loaded", function() {
-		try {
-			for (var i = 0; i < argLen; i++) {
-				var module = arg[i];
-				if (module && module.init && typeof module.init == 'function')
-					module.init();
-			}
-
-			// IE6背景半透明修复
-			DD_belatedPNG.fix('.pngfix');
-		} catch(e) {
-			alert(e);
+		for (var i = 0; i < argLen; i++) {
+			var module = arg[i];
+			if (module && module.init && typeof module.init == 'function')
+				module.init();
 		}
+
+		// IE6背景半透明修复
+		DD_belatedPNG.fix('.pngfix');
 	});
 });
--- public/js/modules/base.js
+++ public/js/modules/base.js
@@ -7,7 +7,6 @@
 define(function(require, exports, module) {
 
 	exports.init = function() {
-		
 	};
-	
+
 });
