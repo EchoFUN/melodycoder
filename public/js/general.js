16fdf9b5e1e2ef864662d4d44edb3277e51ef584
EchoFUN <xukai.ken@gmail.com>
2013-01-06 13:47:53 +0800

    Add a new base script.

--- public/js/general.js
+++ public/js/general.js
@@ -5,25 +5,26 @@
 
 seajs.config({
 	alias : {
-		'prototype' : YYMG.staticUrl + '/js/libs/prototype.js'
+		'prototype' : YYMG.staticUrl + '/js/libs/prototype.js',
 	}
 });
 
 YYMG.modules.push('prototype');
+YYMG.modules.push('./js/modules/base');
 seajs.use(YYMG.modules, function() {
 	var arg = arguments, argLen = arg.length;
 	document.observe("dom:loaded", function() {
 		try {
-			for(var i=0; i<argLen; i++) {
+			for (var i = 0; i < argLen; i++) {
 				var module = arg[i];
-				if(module && module.init && typeof module.init == 'function')
+				if (module && module.init && typeof module.init == 'function')
 					module.init();
 			}
-			
+
 			// IE6背景半透明修复
 			DD_belatedPNG.fix('.pngfix');
 		} catch(e) {
 			alert(e);
 		}
 	});
-}); 
+});
---
+++ public/js/modules/base.js
@@ -0,0 +1,12 @@
+/**
+ * @fileoverview 基础的页面模块
+ * @author xukai.ken@gmail.com
+ *
+ */
+
+define(function(require, exports, module) {
+
+	exports.init = function() {
+	};
+	
+});
