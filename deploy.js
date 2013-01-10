586f5ddf2af47f8be3ff06b7057e3911e000e6d0
EchoFUN <xukai.ken@gmail.com>
2013-01-10 21:15:07 +0800

    add the deployment script.

---
+++ deploy.js
@@ -0,0 +1,17 @@
+/**
+ * Melodycoder
+ * http://botobe.net/
+ * 网站上线自动打包部署脚本，包括压缩合并css等等
+ *
+ * Copyright (c) 2013 Kai.XU
+ * Licensed under the MIT license.
+ */
+
+// 使用基础包
+var fs = require('fs'), path = require('path');
+
+// 调用压缩css和js的工具包
+var uglifyJS = require('uglifyjs'), cleanCSS = require('clean-css');
+
+
+
