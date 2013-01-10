f16412191e93333002d0b4b9a28769932b810726
EchoFUN <xukai.ken@gmail.com>
2013-01-10 21:21:29 +0800

    dev

--- deploy.js
+++ deploy.js
@@ -1,7 +1,7 @@
 /**
  * Melodycoder
  * http://botobe.net/
- * 网站上线自动打包部署脚本，包括压缩合并css等等
+ * 网站上线自动打包部署脚本，包括压缩合并css等等。
  *
  * Copyright (c) 2013 Kai.XU
  * Licensed under the MIT license.
@@ -12,6 +12,8 @@ var fs = require('fs'), path = require('path');
 
 // 调用压缩css和js的工具包
 var uglifyJS = require('uglify-js'), cleanCSS = require('clean-css');
+
+
 
 
 
