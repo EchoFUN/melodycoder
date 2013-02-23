a4b5f530f84c731977ed1f1581323515733f6788
XU Kai <xukai.ken@gmail.com>
2013-02-23 12:25:04 +0800

    完善melodycolder的开发环境和生产环境的分离。

--- app.js
+++ app.js
@@ -6,37 +6,37 @@
  * Licensed under the MIT license.
  */
 
-var express = require('express'), 
-    route = require('./RouterMap'), 
-    http = require('http'), 
-    path = require('path'), 
-    db = require('./model/db'), 
-    config = require('./config').config; 
- 
+var express = require('express'), route = require('./RouterMap'), http = require('http'), path = require('path'), db = require('./model/db'), config = require('./config').config;
+
 var app = express();
 
-app.configure(function() { 
-    app.use(express.cookieParser());
-    app.use(express.session({secret: config.SESSION_SECRET}));
-    app.use(db.initialize());
-    app.set('port', process.env.PORT || 3000);
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'ejs');
-    app.use(express.favicon());
-    // app.use(express.logger('dev'));
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(app.router);
-    app.use(require('stylus').middleware(__dirname + '/public'));
-    app.use(express.static(path.join(__dirname, 'public')));
+app.configure(function() {
+	app.use(express.cookieParser());
+	app.use(express.session({
+		secret : config.SESSION_SECRET
+	}));
+	app.use(db.initialize());
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'ejs');
+	app.use(express.favicon());
+	// app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(require('stylus').middleware(__dirname + '/public'));
+	app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.configure('development', function(){
-    app.use(express.errorHandler()); 
+// 开发环境
+app.configure('development', function() {
+	app.use(express.errorHandler());
+});
+
+// 生产环境
+app.configure（'production', function() {
+
 });
 
 route(app);
-
-http.createServer(app).listen(app.get('port'), function(){
-    console.log("Melody coder is started at " + config.site.SITE_BASE_URL);
-});
+http.createServer(app).listen(app.get('port'));
