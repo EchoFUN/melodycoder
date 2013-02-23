0c685b85da37d9a924bca74fdb11ad865ef606ea
XU Kai <xukai.ken@gmail.com>
2013-02-23 12:38:34 +0800

    配置环境。

--- app.js
+++ app.js
@@ -19,8 +19,6 @@ app.configure(function() {
 	app.set('port', process.env.PORT || 3000);
 	app.set('views', __dirname + '/views');
 	app.set('view engine', 'ejs');
-	app.use(express.favicon());
-	// app.use(express.logger('dev'));
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
 	app.use(app.router);
@@ -30,6 +28,7 @@ app.configure(function() {
 
 // 开发环境
 app.configure('development', function() {
+	app.use(express.logger('dev'));
 	app.use(express.errorHandler());
 });
 
