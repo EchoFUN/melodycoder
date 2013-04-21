bac52171ed9f95470d0f70c7b75269f0cbfa6387
XU Kai <xukai.ken@gmail.com>
2013-04-22 00:20:31 +0800

    试验了下webstorm的格式化代码的功能。

--- routes/post.js
+++ routes/post.js
@@ -6,89 +6,89 @@
 var config = require('../config').config, fs = require('fs'), path = require('path'), async = require('async');
 
 var dbEvt;
-var _middle = function(req, resp, func) {
-	dbEvt = req.dbEvt;
+var _middle = function (req, resp, func) {
+    dbEvt = req.dbEvt;
 
-	var callbackStack = {
-		menus : function(callback) {
-			dbEvt.getAllMenus(callback)
-		},
-		links : function(callback) {
-			dbEvt.getLinks(callback);
-		},
-		rectPosts : function(callback) {
-			dbEvt.getRectPosts(callback);
-		},
-		archives : function(callback) {
-			dbEvt.getArchives(callback);
-		},
-		widgetTags : function(callback) {
-			dbEvt.getWidgetTags(callback);
-		}
-	};
+    var callbackStack = {
+        menus: function (callback) {
+            dbEvt.getAllMenus(callback)
+        },
+        links: function (callback) {
+            dbEvt.getLinks(callback);
+        },
+        rectPosts: function (callback) {
+            dbEvt.getRectPosts(callback);
+        },
+        archives: function (callback) {
+            dbEvt.getArchives(callback);
+        },
+        widgetTags: function (callback) {
+            dbEvt.getWidgetTags(callback);
+        }
+    };
 
-	async.parallel(callbackStack, function(error, result) {
-		func(result);
-	});
-}
+    async.parallel(callbackStack, function (error, result) {
+        func(result);
+    });
+};
 
-exports.index = function(req, resp) {
-	_middle(req, resp, function(result) {
-		var pid = req.query.pid;
-		if (pid) {
-			dbEvt.getPostById(pid, function(post) {
-				var ugcCss = [];
-				if (post) {
-					var tpth = '/css/ugc/' + post._id.toString() + '.css';
+exports.index = function (req, resp) {
+    _middle(req, resp, function (result) {
+        var pid = req.query.pid;
+        if (pid) {
+            dbEvt.getPostById(pid, function (post) {
+                var ugcCss = [];
+                if (post) {
+                    var tpth = '/css/ugc/' + post._id.toString() + '.css';
 
-					if (fs.existsSync(path.resolve('./public' + tpth)))
-						ugcCss.push(tpth);
-				}
-				data = {
-					post : post,
-					result : result,
-					vtype : 4,
-					site : config.site,
-					url : req.url,
-					ugcCss : ugcCss,
-					env : process.env.NODE_ENV
-				};
+                    if (fs.existsSync(path.resolve('./public' + tpth)))
+                        ugcCss.push(tpth);
+                }
+                data = {
+                    post: post,
+                    result: result,
+                    vtype: 4,
+                    site: config.site,
+                    url: req.url,
+                    ugcCss: ugcCss,
+                    env: process.env.NODE_ENV
+                };
 
-				resp.render('index', data);
-			});
-		}
-	});
-}
+                resp.render('index', data);
+            });
+        }
+    });
+};
 
-exports.addComment = function(req, resp) {
-	var ret = {
-		status : {
-			code : 0
-		},
-		data : {
-			time : new Date().getTime()
-		}
-	};
-	dbEvt.addComment(req.body, function(code, isApproved, content) {
-		ret.status.code = code;
-		ret.status.content = content || '';
-		ret.data.isApproved = isApproved || false;
-		resp.end(JSON.stringify(ret));
-	});
-}
+exports.addComment = function (req, resp) {
+    var ret = {
+        status: {
+            code: 0
+        },
+        data: {
+            time: new Date().getTime()
+        }
+    };
+    dbEvt.addComment(req.body, function (code, isApproved, content) {
+        ret.status.code = code;
+        ret.status.content = content || '';
+        ret.data.isApproved = isApproved || false;
+        resp.end(JSON.stringify(ret));
+    });
+};
 
-exports.publishPost = function(req, resp) {
-	dbEvt = req.dbEvt;
-	var ret = {
-		status : {
-			code : 0
-		}
-	}
-	try {
-		var postData = JSON.parse(req.body.r);
-	} catch (e) {
-		throw Error('Error parsing the request.');
-	}
-	ret.status.code = dbEvt.addPost(postData);
-	resp.end(JSON.stringify(ret));
-}
+exports.publishPost = function (req, resp) {
+    dbEvt = req.dbEvt;
+    var ret = {
+        status: {
+            code: 0
+        }
+    };
+    try {
+        var postData = JSON.parse(req.body.r);
+    } catch (e) {
+        throw Error('Error parsing the request.');
+    }
+    ret.status.code = dbEvt.addPost(postData);
+    resp.end(JSON.stringify(ret));
+};
