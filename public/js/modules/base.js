6770053643c896ced7c8b6945186a4b0f76bf073
XU Kai <xukai.ken@gmail.com>
2013-08-05 13:11:34 +0800

    Modify the subtitle, and add the flash effect.

--- public/js/modules/base.js
+++ public/js/modules/base.js
@@ -5,51 +5,56 @@
  */
 
 define(['l/jquery', 'c/components'], function(_, compts) {
-    var Dialog = compts.Dialog;
-    
-    var init = function() {
-        var navigatorEl = _('#navigator'), scrolltopEl = _('#scrolltop'), headerEl = _('#header');
+   var Dialog = compts.Dialog;
 
-        _(window).bind('scroll', function(evt) {
-            var self = _(this);
+   var init = function() {
+      var navigatorEl = _('#navigator'), scrolltopEl = _('#scrolltop'), headerEl = _('#header'), flashLiteEl = _('#flashLite');
 
-            var st = self.scrollTop();
-            if (st < 68) {
-                navigatorEl.css('position', 'static');
-                headerEl.css('height', '70px');
-                scrolltopEl.hide();
-            } else {
-                navigatorEl.css('position', 'fixed');
-                headerEl.css('height', '102px');
-                scrolltopEl.show();
-            }
-        });
-        
-        _(window).bind('resize', function() {
-            var dialog = YYMG._tp.dialog;
-            dialog && dialog.resetPos();
-        });
-        
-        _(document).bind('keydown', function(event) {
-            var dialog = YYMG._tp.dialog;
-            if (event.keyCode == 27)
-                dialog && dialog.close();
-        });
-        
-        scrolltopEl.bind('click', function() {
-        	_(document).scrollTop(0);
-        });
-        
-        _(document).delegate('.underConstruct', 'click', function() {
-           new Dialog({
-              content: '哎呀，被你发现啦，程序猿正在抓狂中！',
-              showClose : false,
-              autoHide: 2,
-           });
-        });
-    };
+      _(window).bind('scroll', function(evt) {
+         var self = _(this);
 
-    return {
-        init: init
-    };
-});
+         var st = self.scrollTop();
+         if (st < 68) {
+            navigatorEl.css('position', 'static');
+            headerEl.css('height', '70px');
+            scrolltopEl.hide();
+         } else {
+            navigatorEl.css('position', 'fixed');
+            headerEl.css('height', '102px');
+            scrolltopEl.show();
+         }
+      });
+
+      _(window).bind('resize', function() {
+         var dialog = YYMG._tp.dialog;
+         dialog && dialog.resetPos();
+      });
+
+      _(document).bind('keydown', function(event) {
+         var dialog = YYMG._tp.dialog;
+         if (event.keyCode == 27)
+            dialog && dialog.close();
+      });
+
+      scrolltopEl.bind('click', function() {
+         _(document).scrollTop(0);
+      });
+
+      _(document).delegate('.underConstruct', 'click', function() {
+         new Dialog({
+            content : '哎呀，被你发现啦，程序猿正在抓狂中！',
+            showClose : false,
+            autoHide : 2,
+         });
+      });
+
+      setInterval(function() {
+         var content = _.trim(flashLiteEl.text());
+         (!content) ? flashLiteEl.text('_') : flashLiteEl.text('');
+      }, 500);
+   };
+
+   return {
+      init : init
+   };
+}); 
