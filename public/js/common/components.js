0cd4e10422aaac2e6746361de5ea36c7f4ed84aa
XU Kai <xukai.ken@gmail.com>
2013-09-08 15:32:44 +0800

    Add the information verify.

--- public/js/common/components.js
+++ public/js/common/components.js
@@ -4,128 +4,126 @@
  */
 
 define(['l/jquery', 'l/mustache', 'c/tpl'], function(_, mustache, tpl) {
-    
-    /**
-     * 
-     * @param {Object} opts
-     */
-    var Dialog = function(opts) {
-        var dopts = {
-            top : undefined,
-            left : undefined,
-            autoHide: undefined,
-            title : '提示',
-            frameTPL : tpl.dialogTPL,
-            content : '',
-            showClose : true
-        };
 
-        opts = _.extend(dopts, opts);
-        this._opts = opts;
-        this._ready();
-        this._constructFrame();
-        YYMG._tp.dialog = this;
-        return this;
-    };
-    
-    Dialog.prototype._ready = function() {
-        
-        // 如果已经存在则删除
-        var dialog = YYMG._tp.dialog;
-        if (dialog)
-          dialog.close();
-        
-        this._dialogEl = document.createElement('div');
-        this._dialogEl.className = 'dialog';
+  /**
+   *
+   * @param {Object} opts
+   */
+  var Dialog = function(opts) {
+    var dopts = {
+      top : undefined,
+      left : undefined,
+      autoHide : undefined,
+      title : '提示',
+      frameTPL : tpl.dialogTPL,
+      content : '',
+      showClose : true
     };
 
-    Dialog.prototype._constructFrame = function() {
-        var opts = this._opts;
+    opts = _.extend(dopts, opts);
+    this._opts = opts;
+    this._ready();
+    this._constructFrame();
+    YYMG._tp.dialog = this;
+    return this;
+  };
 
-        var dialogEl = _(this._dialogEl);
-        
-        var frameHTML = mustache.render(opts.frameTPL, {
-            title : opts.title,
-            content:　opts.content
-        });
+  Dialog.prototype._ready = function() {
 
-        dialogEl.html(frameHTML);
-        
-        _(document.body).append(dialogEl);
-        this.resetPos();
-        
-        // 获取footer部分
-        this._footerEl = dialogEl.find('.dialog-footer');
-        
-        // 设置关闭按钮
-        var close = this._closeEl = dialogEl.find('.close');                
-        var self = this;
-        if (opts.showClose) {
-            close.bind('click', function() {
-                self.close();
-            });
-        } else {
-            close.remove();
-        }
-        
-        // 设置自动隐藏
-        var ah = opts.autoHide;
-        if (ah) {
-            if (typeof ah == 'number') {
-                this.autoHide = window.setTimeout(function() {
-                    self.close();
-                }, ah * 1000);
-            }
-        }
-    };
-    
-    Dialog.prototype.setWidth = function() {
-        return this;  
-    };
-    
-    Dialog.prototype.setContent = function() {
-        return this;  
-    };
-    
-    Dialog.prototype.addButton = function() {
-        var button = document.createElement('a');
-        button.className = 'button';
-        button.href = 'javascript:void(0);';
-        
-        if (typeof callback == 'function') {
-            var self = this;
-            button.bind('click', function() {
-                callback.call(self);
-            });
-        }
-        
-        this._footerEl.append(button);
-        return this;
-    };
-    
-    Dialog.prototype.resetPos = function() {
-        var dialogEl = _(this._dialogEl);
+    // 如果已经存在则删除
+    var dialog = YYMG._tp.dialog;
+    if (dialog)
+      dialog.close();
 
-        var _diaW = dialogEl.css('width'), _diaH = dialogEl.css('height');
+    this._dialogEl = document.createElement('div');
+    this._dialogEl.className = 'dialog';
+  };
 
-        var vp = _(window);
-        var left = this._opts.left || (vp.width() - parseInt(_diaW)) / 2, top = this._opts.top || vp.scrollTop() + (vp.height() - parseInt(_diaH)) / 2;
-        dialogEl.css({
-            top : top + 'px',
-            left : left + 'px'
-        });
-        return this;
-    };
-    
-    Dialog.prototype.close = function() {
-        _(this._dialogEl).remove();
-        this.autoHide && clearTimeout(this.autoHide);
-        YYMG._tp.dialog = null;
-    };
-    
-    
+  Dialog.prototype._constructFrame = function() {
+    var opts = this._opts;
 
-    return {
-        Dialog: Dialog
-    };
+    var dialogEl = _(this._dialogEl);
+    
+    var frameHTML = mustache.render(opts.frameTPL, {
+      title : opts.title,
+      content:　opts.content
+    });
+
+    dialogEl.html(frameHTML);
+
+    _(document.body).append(dialogEl);
+    this.resetPos();
+
+    // 获取footer部分
+    this._footerEl = dialogEl.find('.dialog-footer');
+
+    // 设置关闭按钮
+    var close = this._closeEl = dialogEl.find('.close');
+    var self = this;
+    if (opts.showClose) {
+      close.bind('click', function() {
+        self.close();
+      });
+    } else {
+      close.remove();
+    }
+
+    // 设置自动隐藏
+    var ah = opts.autoHide;
+    if (ah) {
+      if ( typeof ah == 'number') {
+        this.autoHide = window.setTimeout(function() {
+          self.close();
+        }, ah * 1000);
+      }
+    }
+  };
+
+  Dialog.prototype.setWidth = function() {
+    return this;
+  };
+
+  Dialog.prototype.setContent = function() {
+    return this;
+  };
+
+  Dialog.prototype.addButton = function() {
+    var button = document.createElement('a');
+    button.className = 'button';
+    button.href = 'javascript:void(0);';
+
+    if ( typeof callback == 'function') {
+      var self = this;
+      button.bind('click', function() {
+        callback.call(self);
+      });
+    }
+
+    this._footerEl.append(button);
+    return this;
+  };
+
+  Dialog.prototype.resetPos = function() {
+    var dialogEl = _(this._dialogEl);
+
+    var _diaW = dialogEl.css('width'), _diaH = dialogEl.css('height');
+
+    var vp = _(window);
+    var left = this._opts.left || (vp.width() - parseInt(_diaW)) / 2, top = this._opts.top || vp.scrollTop() + (vp.height() - parseInt(_diaH)) / 2;
+    dialogEl.css({
+      top : top + 'px',
+      left : left + 'px'
+    });
+    return this;
+  };
+
+  Dialog.prototype.close = function() {
+    _(this._dialogEl).remove();
+    this.autoHide && clearTimeout(this.autoHide);
+    YYMG._tp.dialog = null;
+  };
+
+  return {
+    Dialog : Dialog
+  };
 });
--- public/js/common/utils.js
+++ public/js/common/utils.js
@@ -6,4 +6,20 @@
 
 define(function(require, exports, module) {
 
+  var regExpress = {
+
+    // 邮箱
+    EMAIL : /^[a-zA-Z0-9_\-\.]{1,}@[a-zA-Z0-9_\-]{1,}\.[a-zA-Z0-9_\-.]{1,}$/,
+
+    // 昵称
+    NICKNAME : /^[\u4e00-\u9fa5\w_]+$/i,
+
+    // 网站地址
+    URL : /^(http)s?:\/\/(?:[\w-]+\.?)+[\.\/\?%&=#@\[\]\-+_!:*~\w-]+|(www)\.(?:[\w-]+\.?)+[\.\/\?%&=#@\[\]\-+_!:*~\w-]+$/i
+  };
+
+  return {
+    regs : regExpress
+  };
+
 });
