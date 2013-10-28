09e87f4eb3a27aa920ccce8462d90fd29da80cce
EchoFUN <xukai.ken@gmail.com>
2013-10-28 17:48:38 +0800

    Format the config file.

--- config.js
+++ config.js
@@ -1,31 +1,31 @@
 exports.config = {
-	site : {
+  site : {
 
-		// 网站名称
-		SITE_NAME : '音韵码工',
+    // 网站名称
+    SITE_NAME : '音韵码工',
 
-		// 网站最基本的URL地址
-		SITE_BASE_URL : 'http://botobe.net',
-      // SITE_BASE_URL : 'http://127.0.0.1:3000',
+    // 网站最基本的URL地址
+    SITE_BASE_URL : 'http://botobe.net',
+    // SITE_BASE_URL : 'http://127.0.0.1:3000',
 
-		// 站点静态网址
-		SITE_STATIC_URL : 'http://melodycoder.duapp.com/dist/v15',
-      // SITE_STATIC_URL : 'http://127.0.0.1:3000',
+    // 站点静态网址
+    SITE_STATIC_URL : 'http://melodycoder.duapp.com/dist/v15',
+    // SITE_STATIC_URL : 'http://127.0.0.1:3000',
 
-		// 文章上传接口
-		ARTICLE_PUBLISH_URL : '/publish',
- 
-		// 每页显示的文章数目
-		PAGE_COUNT : 10
-	},
+    // 文章上传接口
+    ARTICLE_PUBLISH_URL : '/publish',
 
-	db : {
- 
-		DB_HOST : '127.0.0.1',
+    // 每页显示的文章数目
+    PAGE_COUNT : 10
+  },
 
-		// 数据库名称
-		DB_NAME : 'yymg'
-	},
+  db : {
 
-	SESSION_SECRET : '音韵码工'
-};
+    DB_HOST : '127.0.0.1',
+
+    // 数据库名称
+    DB_NAME : 'yymg'
+  },
+
+  SESSION_SECRET : '音韵码工'
+}; 
